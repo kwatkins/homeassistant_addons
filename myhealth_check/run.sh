03501b97f7c446371a2a3ec8154d95b3c73482cb
kevin <kwatkins@gmail.com>
2025-07-17 10:19:47 -0700

    why

--- myhealth_check/run.sh
+++ myhealth_check/run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/with-contenv bashio
+#!/usr/bin/with-contenv bashio
 
 bashio::log.info "Getting configuration..."
 DEFAULT_PORT=$(bashio::config 'port')
 
 bashio::log.info "Starting health check..."
 
-python3 server.py
+python3 /server.py
