85b70bac79b30e31a241009183f1d8c13bb132b3
kevin <kwatkins@gmail.com>
2025-07-17 09:19:01 -0700

    latest

--- myhealth_check/run.sh
+++ myhealth_check/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
+bashio::log.info "Getting configuration..."
+DEFAULT_PORT=$(bashio::config 'port')
+
 bashio::log.info "Starting health check..."
 
 python3 server.py
